21f0a58920ce0909b4ad904ce4394557636127b7
Daniel Fanelli Jr <daniel.r.fanellijr@gmail.com>
2017-06-08 21:55:45 -0700

    Lesson 22

---
+++ Lesson22/DanielStore.cs
@@ -0,0 +1,74 @@
+﻿using BookStore.Instructor;
+using System;
+using BookStore.Instructor.Models;
+
+namespace BookStore.DanielStore
+{
+    public class DanielStore :IBookStoreService
+    {
+        public static IBookStoreService GetService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddAuthor(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddBook(Guid authorId, string title, string ISBN, decimal price, BookShipmentEnum bookShipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddCustomer(string firstName, string lastName, string street, string city, string state, string zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddOrder(Guid customerId, Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBook(Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWarehouseBookQty(Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomerAddress(Guid customerId, string street, string city, string state, string zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderStatus(Guid orderId, OrderStatusEnum orderStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWarehouseBookQty(Guid bookId, BookShipmentEnum bookShipment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
